764535d3ef98472f5f366385bd3475987e9cdfda
Pedro <rogenun52@hotmail.com>
2024-03-26 09:07:24 -0300

    gsgsghsdhds

--- aula dia 26.3/exemplo_heranca.js
+++ aula dia 26.3/exemplo_heranca.js
@@ -5,7 +5,26 @@ class Conta{
 
     get Saldo(){return this.Saldo;}
     set Saldo(pSaldo){this.saldo = pSaldo;}
+
+    imprimir(){
+        return "Saldo: " + this.saldo;
+    }
+}
+
+class Corrente extends Conta {
+    constructor(pLimite){
+        super();
+        this.Limite = pLimite;
+    }
+    get Limite(){return this.Limite;}
+    set Limite(pLimite){this.limite = pLimite;}
 }
 
 var obj_conta = new Conta();
-console.log(obj_conta.saldo);
+obj_conta.saldo = 700;
+console.log(obj_conta.imprimir());
+
+/* var obj_cc = new Corrente(1000);
+obj_cc.saldo = 700;
+console.log(obj_cc);*/
+
