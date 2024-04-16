90b06d8cb0e4c4863e0386e75df60de0a4b48fc4
Pedro <rogenun52@hotmail.com>
2024-04-16 10:30:05 -0300

    primadopaqueta

--- Projeto_SMP/server.js
+++ Projeto_SMP/server.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 
-app.set('port',7777); //escolha de uma porta padrao
+
+require('dotenv').config({path:'variaveis.env'});
+
+app.set('port',process.env.PORT || 7777); //escolha de uma porta padrao
 const server = app.listen(app.get('port'),()=>{
     console.log("Servidor rodando na porta: " + server.address().port);
 });
